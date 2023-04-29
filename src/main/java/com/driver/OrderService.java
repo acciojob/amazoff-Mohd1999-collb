adea59590ca7cb03b175a79a21c589c60253966c
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-04-29 09:31:46 +0000

    Autowired added

--- src/main/java/com/driver/OrderService.java
+++ src/main/java/com/driver/OrderService.java
@@ -1,11 +1,13 @@
 package com.driver;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 
 @Service
 public class OrderService {  
-
+    
+    @Autowired
     OrderRepository orderRepository;        
     public void orderAdded(Order order){
         orderRepository.order_Added(order);
@@ -46,8 +48,6 @@ public class OrderService {
     public Integer getOrders_LeftAfterGivenTimeByPartnerId(String time, String partnerId){
         return orderRepository.getOrdersLeft_AfterGivenTimeByPartnerId(time, partnerId);
     }
-
-////////////
     public String getLastDelivery_TimeByPartnerId(String partnerId){
         return orderRepository.get_LastDeliveryTimeByPartnerId(partnerId);
     }
@@ -59,13 +59,69 @@ public class OrderService {
     public void delete_OrderById(String orderId){
         orderRepository.deleteOrder_ById(orderId);
     }
-
-
-
-
-
-
-
-
-
 }
+
+// package com.driver;
+
+// import org.springframework.stereotype.Service;
+
+// import java.util.List;
+
+// @Service
+// public class OrderService {
+
+//     OrderRepository orderRepository = new OrderRepository();
+
+//     public void addOrder(Order order) {
+//         orderRepository.addOrder(order);
+//     }
+
+//     public void addPartner(String partnerId) {
+//         orderRepository.addPartner(partnerId);
+//     }
+
+//     public void addOrderPartnerPair(String orderId, String partnerId) {
+//         orderRepository.addOrderPartnerPair(orderId, partnerId);
+//     }
+
+//     public Order getOrderById(String orderId) {
+//         return orderRepository.getOrderById(orderId);
+//     }
+
+//     public DeliveryPartner getPartnerById(String partnerId) {
+//         return orderRepository.getPartnerById(partnerId);
+//     }
+
+//     public Integer getOrderCountByPartnerId(String partnerId) {
+//         return orderRepository.getOrderCountByPartnerId(partnerId);
+//     }
+
+//     public List<String> getOrdersByPartnerId(String partnerId) {
+//         return orderRepository.getOrdersByPartnerId(partnerId);
+//     }
+
+//     public List<String> getAllOrders() {
+//         return orderRepository.getAllOrders();
+//     }
+
+//     public Integer getCountOfUnassignedOrders() {
+//         return orderRepository.getCountOfUnassignedOrders();
+//     }
+
+//     public Integer getOrdersLeftAfterGivenTimeByPartnerId(String time, String partnerId) {
+//         return orderRepository.getOrdersLeftAfterGivenTimeByPartnerId(time, partnerId);
+//     }
+
+//     public String getLastDeliveryTimeByPartnerId(String partnerId) {
+//         return orderRepository.getLastDeliveryTimeByPartnerId(partnerId);
+//     }
+
+//     public void deletePartnerById(String partnerId) {
+//         orderRepository.deletePartnerById(partnerId);
+//     }
+
+//     public void deleteOrderById(String orderId) {
+//         orderRepository.deleteOrderById(orderId);
+//     }
+
+// }
